0ca312b6eaefb8b4d17c2bf20459d2520f27b246
tortamque <90132962+tortamque@users.noreply.github.com>
2024-10-05 19:09:23 +0300

    Implement mazes solving inside the cubit

--- lib/services/calculations_service/calculations_service_cubit.dart
+++ lib/services/calculations_service/calculations_service_cubit.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pathfinder/config/data_sources/remote/swagger_repository.dart';
 import 'package:pathfinder/core/network/app_response.dart';
+import 'package:pathfinder/core/shared/domain/entities/maze_data.dart';
 import 'package:pathfinder/core/shared/domain/entities/maze_response.dart';
+import 'package:pathfinder/core/shared/domain/entities/solved_maze_model.dart';
 import 'package:pathfinder/features/problem_solving/domain/entities/a_star_algorithm.dart';
 
 part 'calculations_service_state.dart';
@@ -14,19 +16,17 @@ class CalculationsServiceCubit extends Cubit<CalculationsServiceState> {
   final SwaggerRepository swaggerRepository;
 
   Future<void> solveTask() async {
-    final result = await getTask();
+    final result = await getTasks();
 
     if (result == null) {
       emit(TaskErrorState());
       return;
     }
 
-    print('maze: ${result.data[0]}');
-    final solved = AStarAlgorithm.solveMaze(result.data[0]);
-    print(solved);
+    solveTasks(result.data);
   }
 
-  Future<MazeResponse?> getTask() async {
+  Future<MazeResponse?> getTasks() async {
     emit(TaskLoadingState(progress: 0));
     final result = await swaggerRepository.getTask();
 
@@ -37,5 +37,19 @@ class CalculationsServiceCubit extends Cubit<CalculationsServiceState> {
     emit(TaskLoadingState(progress: 0.5));
 
     return result.data;
+  }
+
+  void solveTasks(List<MazeData> mazes) {
+    final List<SolvedMazeModel> solvedMazes = [];
+    for (var maze in mazes) {
+      final solved = AStarAlgorithm.solveMaze(maze);
+      solvedMazes.add(solved);
+
+      emit(
+        TaskLoadingState(
+          progress: ((solvedMazes.length / mazes.length) / 2) + 0.5,
+        ),
+      );
+    }
   }
 }
--- lib/services/calculations_service/calculations_service_state.dart
+++ lib/services/calculations_service/calculations_service_state.dart
@@ -8,6 +8,14 @@ final class TaskLoadingState extends CalculationsServiceState {
   final double progress;
 
   TaskLoadingState({required this.progress});
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskLoadingState && runtimeType == other.runtimeType && progress == other.progress;
+
+  @override
+  int get hashCode => progress.hashCode;
 }
 
 final class TaskErrorState extends CalculationsServiceState {}
